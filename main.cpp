fd357f687d3c57f663ea5943a98d8920894dd551
JCVelazco <juan.carlosvr.97@hotmail.com>
2019-12-08 23:41:55 -0600

    add mas implementacion y join between all conecciones y metodos del programa

--- main.cpp
+++ main.cpp
@@ -222,11 +222,12 @@ void drawCardAndPlay(std::vector<Card>& player, std::vector<Card>& discardPile, 
 	//SORT CARDS AGAIN
 }
 
-void giveCardsAtStart(std::vector<Card>& player,  std::vector<Card>& drawPile) {
-	for (int i = 0; i < 7; i++)
+void giveCardsAtStart(std::vector<Card>& player,  std::vector<Card>& drawPile, int numberOfCardsToGive) {
+	for (int i = 0; i < numberOfCardsToGive; i++)
 	{
 		Card drawnCard = drawPile.at(0);
 		player.insert(player.begin(), drawnCard);
+		drawPile.erase(drawPile.begin());
 	}
 }
 
@@ -244,19 +245,27 @@ void printInstructivo() {
 	std::cout << "Objetivo del juego\n El objetivo de UNO es deshacerse de todas las cartas que se “roban” inicialmente, diciendo la palabra “UNO” cuando queda la última carta en la mano.El primero que llega a 500 puntos gana.Se recibe puntos por todas las cartas que los otros jugadores todavía tienen en sus manos(véase puntos).\nLa preparación del juego\nSe baraja las cartas y cada jugador recibe siete cartas.Las cartas restantes se ponen encubiertas en el centro y forman el mazo.La primera carta se desvela y se pone al lado.Este mazo es el mazo de descartes.Un jugador se sortea y empieza la ronda.\nDecurso del juego\nEl primero jugador pone una carta de su mano al mazo de descartes.Aquí vale : Una carta sólo se puede superponer en una carta del mismo color o del mismo número.Las cartas negras son cartas de acción especiales con reglas particulares(ver cartas de acción).Si un jugador no puede poner la carta oportuna, tiene que tomar una carta de pena del mazo.Puede jugar esta carta ahora mismo, si la vale bien.Si no, es el turno del siguiente jugador.Quién pone la penúltima carta, debe decir “UNO”(que significa “Eins”) y señala que tiene sólo una última carta en la mano.Si un jugador lo olvida y el otro lo nota a tiempo(antes de que el siguiente jugador haya tomado o ha depuesto una carta) tiene que tomar dos cartas de pena.El ganador de la ronda es él que depone la última carta.Los puntos se suman y se comienza una nueva ronda.\nCartas de Acción\nEn el juego hay cartas de acción negras con diferentes funciones, que se explican a continuación.CARTA TOMA DOS\nCuando se pone esta carta, el siguiente jugador debe tomar dos cartas y no puede deponer ninguna carta en esta ronda.Esta carta sólo puede superponer en una carta con el color apropiado u otras cartas “toma dos”.Si se revela al principio del juego, las mismas reglas aplican.\nCARTA DE RETORNO\nCon esta carta se cambia la dirección.Si se ha jugado por la izquierda, ahora se juega por la derecha y por la inversa.La carta sólo se puede superponer en una carta con color correspondiente o en una otra carta de retorno.Cuando esta carta se toma en el principio del juego, el dador comienza y entonces el jugador a su derecha continúa.\nCARTA DE INTERMISIÓN\nDespués de poner esta carta, el siguiente jugador será “saltado”.La carta sólo se puede superponer en una carta con color correspondiente o en una otra carta de intermisión.Cuando esta carta se toma en el principio del juego, el jugador se “salta” a la izquierda del dador y el jugador a la izquierda de este jugador continúa el juego.\nCARTA DE ELECCIÓN DE COLORES\nCon esta carta el jugador decide qué color sigue en el juego.También el color presente puede ser seleccionado.Una carta de elección de colores también se puede poner cuando el jugador puede poner una carta diferente.Si se toma una carta de elección de colores en el principio del juego, el jugador a la izquierda del dador decide qué color va a seguir.\nCARTA DE TOMAR CUATRO COLORES Esta carta es la mejor.El jugador decide qué color sigue en el juego.Además, el siguiente jugador debe tomar cuatro cartas.No se puede deponer cualquier carta en esta ronda.Por desgracia, la carta sólo se puede poner si el jugador que la tiene, no tiene ninguna carta en la mano que corresponde con el color del montón.Si el jugador tiene una carta con el número o una carta de acción, sin embargo la carta de tomar cuatro colores se puede poner.Un jugador que tiene una carta de tomar cuatro colores puede tirarse un farol y poner esa carta prohibida.Si el jugador se atrapa ciertas reglas aplican(ver cartas de pena).Si esta carta se toma en el principio del juego, se devuelve en el mazo y se toma otra carta.\nPenas\nSi un jugador no respeta las reglas, debe tomar una o más cartas de pena.Las reglas son los siguientes :\n \tUNO: Si un jugador olvida llamar “UNO” después de su penúltima carta y el siguiente jugador aún no ha jugado su carta, debe tomar una carta de pena.\n \tPropuestas : Quién hace propuestas a sus jugadores, debe tomar dos cartas de pena.\n \tPuesto incorrecto : Quién pone una carta, a pesar de que no está o una carta incorrecta, debe reanudarla y además, recibe una carta de pena.\n \tLa + 4 sólo se puede poner cuando el jugador no puede utilizar el color actual con excepción de otras cartas de acción.Si el jugador que tiene la + 4 cree que la carta se ha jugado mal, puede desafiar el jugador anterior.Debe entonces justificar al mostrar sus cartas que en realidad no podía operar correctamente el color.Puede confirmarlo, el jugador desafiante ahora debe tomar seis cartas en lugar de cuatro.Sin embargo fue trasladado a tener la + 4 ilegalmente, debe tomar las cuatro cartas a su mismo.";
 }
 
-void selectOptionOfMenu(int numberOfPlayer, std::vector<Card> actualPlayerDeck) {
+void selectOptionOfMenu(int numberOfPlayer, std::vector<Card>& actualPlayerDeck, std::vector<Card>& drawPile, std::vector<Card>& discardPile) {
 	int choice;
+	// if es el player imprime el menu
+	if(numberOfPlayer == 1)
 	printMenu();
 
-	std::cin >> choice;
+	// si es player, se le dara 5 segundos para contestar que quiere hacer
+	if (numberOfPlayer) {
+		std::cin >> choice;
+	}
+	else {
+		// si es cpu, checara con nun metodo si tiene una carta
+		std::string myCard = metodoMagico(actualPlayerDeck, discardPile);
+		playCard(myCard, actualPlayerDeck, discardPile);
+	}
+	
 
 	switch (choice) {
 	case 1:
 		std::cout << "You choose: DRAW\n";
-		// add function to draw card
-		drawCardAndPlay();
-		// draw card function() and in authomatic run function ifCardWorksPlayIt()
-		// finishTurn()
+		drawCardAndPlay(actualPlayerDeck, discardPile, drawPile);
 		break;
 
 	case 2:
@@ -270,12 +279,22 @@ void selectOptionOfMenu(int numberOfPlayer, std::vector<Card> actualPlayerDeck) 
 	case 3:
 		std::cout << "You choose: PLAYCARD\n";
 		// check if the the user had one card... if yes, punish him for not saying UNO
-		//no se que va adentro de playCard
-		try {
-			playCard();
-		}
-		catch (const char* msg) {
-			std::cout << msg << std::endl;
+		if (actualPlayerDeck.size() == 1) {
+			// dumb, and the UNOOOOO?
+			// give 2 cards to this dumb guy
+			giveCardsAtStart(actualPlayerDeck,drawPile, 2);
+		}else {
+			try {
+				std::string carta;
+				std::cout << "Que carta deseas jugar? (Escribela exactamente como la vez)\n";
+				std::cin >> carta;
+				playCard(carta, actualPlayerDeck, discardPile);
+			}
+			catch (const char* msg) {
+				std::cout << msg << std::endl;
+				std::cout << "La carta que mandaste fue incorrecta, elige de nuevo: \n";
+				selectOptionOfMenu(numberOfPlayer, actualPlayerDeck, drawPile, discardPile);
+			}
 		}
 
 		// check if win()
@@ -285,7 +304,7 @@ void selectOptionOfMenu(int numberOfPlayer, std::vector<Card> actualPlayerDeck) 
 	case 4:
 		std::cout << "You choose: HELP\n";
 		printInstructivo();
-		selectOptionOfMenu(numberOfPlayer, actualPlayerDeck);
+		selectOptionOfMenu(numberOfPlayer, actualPlayerDeck, drawPile, discardPile);
 		break;
 
 	case 5:
@@ -295,7 +314,7 @@ void selectOptionOfMenu(int numberOfPlayer, std::vector<Card> actualPlayerDeck) 
 
 	default:
 		std::cout << "You choose: Error, option not recognized... try again.\n";
-		selectOptionOfMenu(numberOfPlayer, actualPlayerDeck);
+		selectOptionOfMenu(numberOfPlayer, actualPlayerDeck, drawPile, discardPile);
 		break;
 	}
 
@@ -318,7 +337,14 @@ void playCard(std::string card, std::vector<Card>& player, std::vector<Card>& di
 	int value = findNumber(card);
 	Card mycard(color, value, type);
 	//Delete card from user's cards
-	player.erase(std::remove(player.begin(), player.end(), mycard), player.end());
+	std::vector<Card>::iterator itr = std::find(player.begin(), player.end(), mycard);
+
+	if (itr != player.cend()) {
+		player.erase(player.begin() + std::distance(player.begin(), itr));
+	}
+	else {
+		throw "Element not found";
+	}
 	//Add the new card(same card) to the discard pile
 	discardPile.insert(discardPile.begin(), mycard);
 }
@@ -359,7 +385,7 @@ int main(int argc, char const* argv[])
 		displayTopCard(drawPile);
 
 		std::vector<Card> actualV = (turn == 1) ? playerOne : (turn == 2) ? cpuOne : cpuTwo;
-		selectOptionOfMenu(turn, actualV);
+		selectOptionOfMenu(turn, actualV, drawPile, discardPile);
 	}
 	return 0;
 }
