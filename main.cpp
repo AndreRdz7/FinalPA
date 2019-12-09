d918d05d4846dd0fa1ab2232d0ca99e9c89bf851
AndreRdz7 <A01730716@itesm.mx>
2019-12-09 01:54:53 -0600

    medio sort

--- main.cpp
+++ main.cpp
@@ -21,22 +21,26 @@ public:
 		value = cardValue;
 		special = cardSpecial;
 	}
-	bool operator==(const Card& card) {
+	bool operator==(const Card &card)
+	{
 		return card.color == color && card.value == value && card.special == special;
 	}
 };
 
-std::string findType(std::string card) {
+std::string findType(std::string card)
+{
 	if (card.find("REVERSE") != std::string::npos)
 		return "REVERSE";
 	else if (card.find("BLOCK") != std::string::npos)
 		return "BLOCK";
 	else if (card.find("PLUSTWO") != std::string::npos)
 		return "PLUSTWO";
-	else return "REGULAR";
+	else
+		return "REGULAR";
 }
 
-std::string findColor(std::string card) {
+std::string findColor(std::string card)
+{
 	if (card.find("YELLOW") != std::string::npos)
 		return "YELLOW";
 	else if (card.find("RED") != std::string::npos)
@@ -45,10 +49,12 @@ std::string findColor(std::string card) {
 		return "BLUE";
 	else if (card.find("GREEN") != std::string::npos)
 		return "GREEN";
-	else return "NOTFOUND";
+	else
+		return "NOTFOUND";
 }
 
-int findNumber(std::string card) {
+int findNumber(std::string card)
+{
 	if (card.find("1") != std::string::npos)
 		return 1;
 	else if (card.find("2") != std::string::npos)
@@ -69,13 +75,14 @@ int findNumber(std::string card) {
 		return 9;
 	else if (card.find("0") != std::string::npos)
 		return 0;
-	else return -1;
+	else
+		return -1;
 }
 
-void startGame(std::vector<Card>& drawPile)
+void startGame(std::vector<Card> &drawPile)
 {
-	std::string colors[4] = { "RED", "YELLOW", "BLUE", "GREEN" };
-	std::string specials[3] = { "BLOCK", "PLUSTWO", "REVERSE" };
+	std::string colors[4] = {"RED", "YELLOW", "BLUE", "GREEN"};
+	std::string specials[3] = {"BLOCK", "PLUSTWO", "REVERSE"};
 	for (int i = 0; i < 4; i++)
 	{
 		Card card0(colors[i], 0, "REGULAR");
@@ -96,7 +103,7 @@ void startGame(std::vector<Card>& drawPile)
 	std::cout << "Game with " << drawPile.size() << " cards" << std::endl;
 }
 
-void displayHand(std::vector<Card>& hand)
+void displayHand(std::vector<Card> &hand)
 {
 	int size = hand.size();
 	for (int i = 0; i < size; i++)
@@ -117,7 +124,8 @@ void displayHand(std::vector<Card>& hand)
 	std::cout << std::endl;
 }
 
-void displayTopCard(std::vector<Card>& discardPile) {
+void displayTopCard(std::vector<Card> &discardPile)
+{
 	std::string topCard;
 	if (discardPile.at(0).special == "REGULAR")
 	{
@@ -130,7 +138,7 @@ void displayTopCard(std::vector<Card>& discardPile) {
 	std::cout << "[" << topCard << "]" << std::endl;
 }
 
-void opponentsHand(std::vector<Card>& hand, int numberOfPlayer)
+void opponentsHand(std::vector<Card> &hand, int numberOfPlayer)
 {
 	int size = hand.size();
 	std::string handDisplay;
@@ -138,14 +146,20 @@ void opponentsHand(std::vector<Card>& hand, int numberOfPlayer)
 	std::cout << handDisplay << std::endl;
 }
 
-void shuffle(std::vector<Card>& drawPile)
+void shuffle(std::vector<Card> &drawPile)
 {
 	auto rng = std::default_random_engine{};
 	std::shuffle(std::begin(drawPile), std::end(drawPile), rng);
 }
 
-void sortHand(std::vector<Card> hand)
+bool compare(const Card &card1, const Card &card2)
 {
+	return card1.value < card2.value;
+}
+
+void sortHand(std::vector<Card> &hand)
+{
+	std::sort(hand.begin(), hand.end(), &compare);
 }
 
 /*
@@ -208,7 +222,8 @@ int getScore(std::vector<Card> firstHand, std::vector<Card> secondHand, int ifh,
 	return totalScore;
 }
 
-void drawCardAndPlay(std::vector<Card>& player, std::vector<Card>& discardPile, std::vector<Card>& drawPile) {
+void drawCardAndPlay(std::vector<Card> &player, std::vector<Card> &discardPile, std::vector<Card> &drawPile)
+{
 	Card drawnCard = drawPile.at(0);
 	Card topCard = discardPile.at(0);
 
@@ -222,7 +237,8 @@ void drawCardAndPlay(std::vector<Card>& player, std::vector<Card>& discardPile, 
 	//SORT CARDS AGAIN
 }
 
-void giveCardsAtStart(std::vector<Card>& player,  std::vector<Card>& drawPile, int numberOfCardsToGive) {
+void giveCardsAtStart(std::vector<Card> &player, std::vector<Card> &drawPile, int numberOfCardsToGive)
+{
 	for (int i = 0; i < numberOfCardsToGive; i++)
 	{
 		Card drawnCard = drawPile.at(0);
@@ -231,8 +247,8 @@ void giveCardsAtStart(std::vector<Card>& player,  std::vector<Card>& drawPile, i
 	}
 }
 
-
-void printMenu() {
+void printMenu()
+{
 	std::cout << "Choose the number of yhe option you want to make:\n";
 	std::cout << "1. DRAW\n";
 	std::cout << "2. UNO\n";
@@ -241,28 +257,32 @@ void printMenu() {
 	std::cout << "5. EXIT\n";
 }
 
-void printInstructivo() {
+void printInstructivo()
+{
 	std::cout << "Objetivo del juego\n El objetivo de UNO es deshacerse de todas las cartas que se “roban” inicialmente, diciendo la palabra “UNO” cuando queda la última carta en la mano.El primero que llega a 500 puntos gana.Se recibe puntos por todas las cartas que los otros jugadores todavía tienen en sus manos(véase puntos).\nLa preparación del juego\nSe baraja las cartas y cada jugador recibe siete cartas.Las cartas restantes se ponen encubiertas en el centro y forman el mazo.La primera carta se desvela y se pone al lado.Este mazo es el mazo de descartes.Un jugador se sortea y empieza la ronda.\nDecurso del juego\nEl primero jugador pone una carta de su mano al mazo de descartes.Aquí vale : Una carta sólo se puede superponer en una carta del mismo color o del mismo número.Las cartas negras son cartas de acción especiales con reglas particulares(ver cartas de acción).Si un jugador no puede poner la carta oportuna, tiene que tomar una carta de pena del mazo.Puede jugar esta carta ahora mismo, si la vale bien.Si no, es el turno del siguiente jugador.Quién pone la penúltima carta, debe decir “UNO”(que significa “Eins”) y señala que tiene sólo una última carta en la mano.Si un jugador lo olvida y el otro lo nota a tiempo(antes de que el siguiente jugador haya tomado o ha depuesto una carta) tiene que tomar dos cartas de pena.El ganador de la ronda es él que depone la última carta.Los puntos se suman y se comienza una nueva ronda.\nCartas de Acción\nEn el juego hay cartas de acción negras con diferentes funciones, que se explican a continuación.CARTA TOMA DOS\nCuando se pone esta carta, el siguiente jugador debe tomar dos cartas y no puede deponer ninguna carta en esta ronda.Esta carta sólo puede superponer en una carta con el color apropiado u otras cartas “toma dos”.Si se revela al principio del juego, las mismas reglas aplican.\nCARTA DE RETORNO\nCon esta carta se cambia la dirección.Si se ha jugado por la izquierda, ahora se juega por la derecha y por la inversa.La carta sólo se puede superponer en una carta con color correspondiente o en una otra carta de retorno.Cuando esta carta se toma en el principio del juego, el dador comienza y entonces el jugador a su derecha continúa.\nCARTA DE INTERMISIÓN\nDespués de poner esta carta, el siguiente jugador será “saltado”.La carta sólo se puede superponer en una carta con color correspondiente o en una otra carta de intermisión.Cuando esta carta se toma en el principio del juego, el jugador se “salta” a la izquierda del dador y el jugador a la izquierda de este jugador continúa el juego.\nCARTA DE ELECCIÓN DE COLORES\nCon esta carta el jugador decide qué color sigue en el juego.También el color presente puede ser seleccionado.Una carta de elección de colores también se puede poner cuando el jugador puede poner una carta diferente.Si se toma una carta de elección de colores en el principio del juego, el jugador a la izquierda del dador decide qué color va a seguir.\nCARTA DE TOMAR CUATRO COLORES Esta carta es la mejor.El jugador decide qué color sigue en el juego.Además, el siguiente jugador debe tomar cuatro cartas.No se puede deponer cualquier carta en esta ronda.Por desgracia, la carta sólo se puede poner si el jugador que la tiene, no tiene ninguna carta en la mano que corresponde con el color del montón.Si el jugador tiene una carta con el número o una carta de acción, sin embargo la carta de tomar cuatro colores se puede poner.Un jugador que tiene una carta de tomar cuatro colores puede tirarse un farol y poner esa carta prohibida.Si el jugador se atrapa ciertas reglas aplican(ver cartas de pena).Si esta carta se toma en el principio del juego, se devuelve en el mazo y se toma otra carta.\nPenas\nSi un jugador no respeta las reglas, debe tomar una o más cartas de pena.Las reglas son los siguientes :\n \tUNO: Si un jugador olvida llamar “UNO” después de su penúltima carta y el siguiente jugador aún no ha jugado su carta, debe tomar una carta de pena.\n \tPropuestas : Quién hace propuestas a sus jugadores, debe tomar dos cartas de pena.\n \tPuesto incorrecto : Quién pone una carta, a pesar de que no está o una carta incorrecta, debe reanudarla y además, recibe una carta de pena.\n \tLa + 4 sólo se puede poner cuando el jugador no puede utilizar el color actual con excepción de otras cartas de acción.Si el jugador que tiene la + 4 cree que la carta se ha jugado mal, puede desafiar el jugador anterior.Debe entonces justificar al mostrar sus cartas que en realidad no podía operar correctamente el color.Puede confirmarlo, el jugador desafiante ahora debe tomar seis cartas en lugar de cuatro.Sin embargo fue trasladado a tener la + 4 ilegalmente, debe tomar las cuatro cartas a su mismo.";
 }
 
-void selectOptionOfMenu(int numberOfPlayer, std::vector<Card>& actualPlayerDeck, std::vector<Card>& drawPile, std::vector<Card>& discardPile) {
+void selectOptionOfMenu(int numberOfPlayer, std::vector<Card> &actualPlayerDeck, std::vector<Card> &drawPile, std::vector<Card> &discardPile)
+{
 	int choice;
 	// if es el player imprime el menu
-	if(numberOfPlayer == 1)
-	printMenu();
+	if (numberOfPlayer == 1)
+		printMenu();
 
 	// si es player, se le dara 5 segundos para contestar que quiere hacer
-	if (numberOfPlayer) {
+	if (numberOfPlayer)
+	{
 		std::cin >> choice;
 	}
-	else {
+	else
+	{
 		// si es cpu, checara con nun metodo si tiene una carta
 		std::string myCard = metodoMagico(actualPlayerDeck, discardPile);
 		playCard(myCard, actualPlayerDeck, discardPile);
 	}
-	
 
-	switch (choice) {
+	switch (choice)
+	{
 	case 1:
 		std::cout << "You choose: DRAW\n";
 		drawCardAndPlay(actualPlayerDeck, discardPile, drawPile);
@@ -279,18 +299,23 @@ void selectOptionOfMenu(int numberOfPlayer, std::vector<Card>& actualPlayerDeck,
 	case 3:
 		std::cout << "You choose: PLAYCARD\n";
 		// check if the the user had one card... if yes, punish him for not saying UNO
-		if (actualPlayerDeck.size() == 1) {
+		if (actualPlayerDeck.size() == 1)
+		{
 			// dumb, and the UNOOOOO?
 			// give 2 cards to this dumb guy
-			giveCardsAtStart(actualPlayerDeck,drawPile, 2);
-		}else {
-			try {
+			giveCardsAtStart(actualPlayerDeck, drawPile, 2);
+		}
+		else
+		{
+			try
+			{
 				std::string carta;
 				std::cout << "Que carta deseas jugar? (Escribela exactamente como la vez)\n";
 				std::cin >> carta;
 				playCard(carta, actualPlayerDeck, discardPile);
 			}
-			catch (const char* msg) {
+			catch (const char *msg)
+			{
 				std::cout << msg << std::endl;
 				std::cout << "La carta que mandaste fue incorrecta, elige de nuevo: \n";
 				selectOptionOfMenu(numberOfPlayer, actualPlayerDeck, drawPile, discardPile);
@@ -317,65 +342,71 @@ void selectOptionOfMenu(int numberOfPlayer, std::vector<Card>& actualPlayerDeck,
 		selectOptionOfMenu(numberOfPlayer, actualPlayerDeck, drawPile, discardPile);
 		break;
 	}
-
 }
 
-bool gameIsRunning(std::vector<Card> player, std::vector<Card> pc1, std::vector<Card> pc2) {
-	if (pc1.empty() || player.empty() || pc2.empty()) {
+bool gameIsRunning(std::vector<Card> player, std::vector<Card> pc1, std::vector<Card> pc2)
+{
+	if (pc1.empty() || player.empty() || pc2.empty())
+	{
 		return false;
 	}
-	else {
+	else
+	{
 		return true;
 	}
 }
 
-
-void playCard(Card mycard, std::vector<Card> &player, std::vector<Card> &discardPile){
-    //Delete card from user's cards
-    player.erase(std::remove(player.begin(), player.end(), mycard), player.end());
-    //Add the new card(same card) to the discard pile
-    discardPile.insert(discardPile.begin(),mycard);
+void playCard(Card mycard, std::vector<Card> &player, std::vector<Card> &discardPile)
+{
+	//Delete card from user's cards
+	player.erase(std::remove(player.begin(), player.end(), mycard), player.end());
+	//Add the new card(same card) to the discard pile
+	discardPile.insert(discardPile.begin(), mycard);
 }
 
-Card checkPlayCardCPU(std::vector<Card> &playercpu, std::vector<Card> &discardPile){
-    Card topCard = discardPile.at(0);
-    for(auto card: playercpu){
-	    if(card.color == topCard.color || card.value == topCard.value || card.special == topCard.special){
-            return card;
-        }
-    }
-    Card mycard("NULL",-2,"NULL");
-    return mycard;
+Card checkPlayCardCPU(std::vector<Card> &playercpu, std::vector<Card> &discardPile)
+{
+	Card topCard = discardPile.at(0);
+	for (auto card : playercpu)
+	{
+		if (card.color == topCard.color || card.value == topCard.value || card.special == topCard.special)
+		{
+			return card;
+		}
+	}
+	Card mycard("NULL", -2, "NULL");
+	return mycard;
 }
 
-Card processStringToCard(std::string card){
-    std::string color = findColor(card);
-    std::string type = findType(card);
-    int value = findNumber(card);
-    Card mycard(color,value,type);
-    return mycard;
+Card processStringToCard(std::string card)
+{
+	std::string color = findColor(card);
+	std::string type = findType(card);
+	int value = findNumber(card);
+	Card mycard(color, value, type);
+	return mycard;
 }
 
-int main(int argc, char const* argv[])
+int main(int argc, char const *argv[])
 {
 	// revisar archivo si hay un juego pendiente, si no, se crea uno:
 	std::vector<Card> drawPile;
 	startGame(drawPile); //put 100 cards in the drawPile
-	shuffle(drawPile); // shuffle it
+	shuffle(drawPile);   // shuffle it
 
 	// discard pile
 	std::vector<Card> discardPile;
 
 	std::vector<Card> playerOne; //the human
-	std::vector<Card> cpuOne; //the pc1
-	std::vector<Card> cpuTwo; //the pc2
+	std::vector<Card> cpuOne;	//the pc1
+	std::vector<Card> cpuTwo;	//the pc2
 
 	//give 7/7/7 cards to each player from the drawPile
 
-
 	// start of game... //each player already has 7 cards...
-	int turn = 1; // 1 -> Player, 2 -> Pc1, 3 -> Pc2 
-	while (gameIsRunning(playerOne, cpuOne, cpuTwo)) {
+	int turn = 1; // 1 -> Player, 2 -> Pc1, 3 -> Pc2
+	while (gameIsRunning(playerOne, cpuOne, cpuTwo))
+	{
 		// play
 
 		// see mi mano
