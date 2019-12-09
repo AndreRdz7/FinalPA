06d375691da9a6f5814e1c89d935945eb4b6d2d7
JCVelazco <juan.carlosvr.97@hotmail.com>
2019-12-08 23:06:36 -0600

    add more logic and join everything

--- main.cpp
+++ main.cpp
@@ -4,140 +4,144 @@
 #include <vector>
 #include <fstream>
 #include <random>
+#include <condition_variable>
+
+std::condition_variable cv;
+int option;
 
 class Card
 {
 public:
-    std::string color;
-    int value;
-    std::string special;
-    Card(std::string cardColor, int cardValue, std::string cardSpecial)
-    {
-        color = cardColor;
-        value = cardValue;
-        special = cardSpecial;
-    }
-        bool operator==(const Card &card) {
+	std::string color;
+	int value;
+	std::string special;
+	Card(std::string cardColor, int cardValue, std::string cardSpecial)
+	{
+		color = cardColor;
+		value = cardValue;
+		special = cardSpecial;
+	}
+	bool operator==(const Card& card) {
 		return card.color == color && card.value == value && card.special == special;
 	}
 };
 
-std::string findType(std::string card){
-    if (card.find("REVERSE")!=std::string::npos)
-        return "REVERSE";
-    else if (card.find("BLOCK")!=std::string::npos)
-        return "BLOCK";
-    else if (card.find("PLUSTWO")!=std::string::npos)
-        return "PLUSTWO";
-    else return "REGULAR";
+std::string findType(std::string card) {
+	if (card.find("REVERSE") != std::string::npos)
+		return "REVERSE";
+	else if (card.find("BLOCK") != std::string::npos)
+		return "BLOCK";
+	else if (card.find("PLUSTWO") != std::string::npos)
+		return "PLUSTWO";
+	else return "REGULAR";
 }
 
-std::string findColor(std::string card){
-    if (card.find("YELLOW")!=std::string::npos)
-        return "YELLOW";
-    else if (card.find("RED")!=std::string::npos)
-        return "RED";
-    else if (card.find("BLUE")!=std::string::npos)
-        return "BLUE";
-    else if (card.find("GREEN")!=std::string::npos)
-        return "GREEN";
-    else return "NOTFOUND";
+std::string findColor(std::string card) {
+	if (card.find("YELLOW") != std::string::npos)
+		return "YELLOW";
+	else if (card.find("RED") != std::string::npos)
+		return "RED";
+	else if (card.find("BLUE") != std::string::npos)
+		return "BLUE";
+	else if (card.find("GREEN") != std::string::npos)
+		return "GREEN";
+	else return "NOTFOUND";
 }
 
-int findNumber(std::string card){
-    if (card.find("1")!=std::string::npos)
-        return 1;
-    else if (card.find("2")!=std::string::npos)
-        return 2;
-    else if (card.find("3")!=std::string::npos)
-        return 3;
-    else if (card.find("4")!=std::string::npos)
-        return 4;
-    if (card.find("5")!=std::string::npos)
-        return 5;
-    else if (card.find("6")!=std::string::npos)
-        return 6;
-    else if (card.find("7")!=std::string::npos)
-        return 7;
-    else if (card.find("8")!=std::string::npos)
-        return 8;
-    else if (card.find("9")!=std::string::npos)
-        return 9;
-    else if (card.find("0")!=std::string::npos)
-        return 0;
-    else return -1;
+int findNumber(std::string card) {
+	if (card.find("1") != std::string::npos)
+		return 1;
+	else if (card.find("2") != std::string::npos)
+		return 2;
+	else if (card.find("3") != std::string::npos)
+		return 3;
+	else if (card.find("4") != std::string::npos)
+		return 4;
+	if (card.find("5") != std::string::npos)
+		return 5;
+	else if (card.find("6") != std::string::npos)
+		return 6;
+	else if (card.find("7") != std::string::npos)
+		return 7;
+	else if (card.find("8") != std::string::npos)
+		return 8;
+	else if (card.find("9") != std::string::npos)
+		return 9;
+	else if (card.find("0") != std::string::npos)
+		return 0;
+	else return -1;
 }
 
-void startGame(std::vector<Card> &drawPile)
+void startGame(std::vector<Card>& drawPile)
 {
-    std::string colors[4] = {"RED", "YELLOW", "BLUE", "GREEN"};
-    std::string specials[3] = {"BLOCK", "PLUSTWO", "REVERSE"};
-    for (int i = 0; i < 4; i++)
-    {
-        Card card0(colors[i], 0, "REGULAR");
-        drawPile.push_back(card0);
-        for (int num = 1; num < 10; num++)
-        {
-            Card card1(colors[i], num, "REGULAR");
-            drawPile.push_back(card1);
-            drawPile.push_back(card1);
-        }
-        for (int esp = 0; esp < 3; esp++)
-        {
-            Card card2(colors[i], -1, specials[esp]);
-            drawPile.push_back(card2);
-            drawPile.push_back(card2);
-        }
-    }
-    std::cout << "Game with " << drawPile.size() << " cards" << std::endl;
+	std::string colors[4] = { "RED", "YELLOW", "BLUE", "GREEN" };
+	std::string specials[3] = { "BLOCK", "PLUSTWO", "REVERSE" };
+	for (int i = 0; i < 4; i++)
+	{
+		Card card0(colors[i], 0, "REGULAR");
+		drawPile.push_back(card0);
+		for (int num = 1; num < 10; num++)
+		{
+			Card card1(colors[i], num, "REGULAR");
+			drawPile.push_back(card1);
+			drawPile.push_back(card1);
+		}
+		for (int esp = 0; esp < 3; esp++)
+		{
+			Card card2(colors[i], -1, specials[esp]);
+			drawPile.push_back(card2);
+			drawPile.push_back(card2);
+		}
+	}
+	std::cout << "Game with " << drawPile.size() << " cards" << std::endl;
 }
 
-void displayHand(std::vector<Card> &hand)
+void displayHand(std::vector<Card>& hand)
 {
-    int size = hand.size();
-    for (int i = 0; i < size; i++)
-    {
-        std::cout << "[";
-        std::string currentCard;
-        if (hand.at(i).special == "REGULAR")
-        {
-            currentCard = hand.at(i).color + std::to_string(hand.at(i).value);
-        }
-        else
-        {
-            currentCard = hand.at(i).color + hand.at(i).special;
-        }
-        std::cout << currentCard;
-        std::cout << "]";
-    }
-    std::cout << std::endl;
+	int size = hand.size();
+	for (int i = 0; i < size; i++)
+	{
+		std::cout << "[";
+		std::string currentCard;
+		if (hand.at(i).special == "REGULAR")
+		{
+			currentCard = hand.at(i).color + std::to_string(hand.at(i).value);
+		}
+		else
+		{
+			currentCard = hand.at(i).color + hand.at(i).special;
+		}
+		std::cout << currentCard;
+		std::cout << "]";
+	}
+	std::cout << std::endl;
 }
 
-void displayTopCard(std::vector<Card> &discardPile){
-    std::string topCard;
-    if (discardPile.at(0).special == "REGULAR")
-    {
-        topCard = discardPile.at(0).color + std::to_string(discardPile.at(0).value);
-    }
-    else
-    {
-        topCard = discardPile.at(0).color + discardPile.at(0).special;
-    }
-    std::cout << "[" << topCard << "]" << std::endl;
+void displayTopCard(std::vector<Card>& discardPile) {
+	std::string topCard;
+	if (discardPile.at(0).special == "REGULAR")
+	{
+		topCard = discardPile.at(0).color + std::to_string(discardPile.at(0).value);
+	}
+	else
+	{
+		topCard = discardPile.at(0).color + discardPile.at(0).special;
+	}
+	std::cout << "[" << topCard << "]" << std::endl;
 }
 
-void opponentsHand(std::vector<Card> &hand, int numberOfPlayer)
+void opponentsHand(std::vector<Card>& hand, int numberOfPlayer)
 {
-    int size = hand.size();
-    std::string handDisplay;
-    handDisplay += "P" + std::to_string(numberOfPlayer) + "->" + std::to_string(size);
-    std::cout << handDisplay << std::endl;
+	int size = hand.size();
+	std::string handDisplay;
+	handDisplay += "P" + std::to_string(numberOfPlayer) + "->" + std::to_string(size);
+	std::cout << handDisplay << std::endl;
 }
 
-void shuffle(std::vector<Card> &drawPile)
+void shuffle(std::vector<Card>& drawPile)
 {
-    auto rng = std::default_random_engine{};
-    std::shuffle(std::begin(drawPile), std::end(drawPile), rng);
+	auto rng = std::default_random_engine{};
+	std::shuffle(std::begin(drawPile), std::end(drawPile), rng);
 }
 
 void sortHand(std::vector<Card> hand)
@@ -145,76 +149,85 @@ void sortHand(std::vector<Card> hand)
 }
 
 /*
-    @params
-    fristHand, secondHand, which are the hands of the 2 players that lost
-    @return
-    integer value of total score
+	@params
+	fristHand, secondHand, which are the hands of the 2 players that lost
+	@return
+	integer value of total score
 */
 int getScore(std::vector<Card> firstHand, std::vector<Card> secondHand, int ifh, int ish)
 {
-    int totalScore = 0;
-    // firstHand
-    if (ifh < firstHand.size())
-    {
-        if (firstHand.at(ifh).value != -1)
-        {
-            totalScore += firstHand.at(ifh).value;
-        }
-        else
-        {
-            if (firstHand.at(ifh).special == "PLUSTWO")
-            {
-                totalScore += 30;
-            }
-            else if (firstHand.at(ifh).special == "REVERSE")
-            {
-                totalScore += 25;
-            }
-            else
-            {
-                totalScore += 20;
-            }
-        }
-        getScore(firstHand, secondHand, ifh + 1, ish);
-    }
-    // secondHand
-    if (ish < secondHand.size())
-    {
-        if (secondHand.at(ish).value != -1)
-        {
-            totalScore += secondHand.at(ish).value;
-        }
-        else
-        {
-            if (secondHand.at(ish).special == "PLUSTWO")
-            {
-                totalScore += 30;
-            }
-            else if (secondHand.at(ish).special == "REVERSE")
-            {
-                totalScore += 25;
-            }
-            else
-            {
-                totalScore += 20;
-            }
-        }
-        getScore(firstHand, secondHand, ifh, ish + 1);
-    }
-    return totalScore;
+	int totalScore = 0;
+	// firstHand
+	if (ifh < firstHand.size())
+	{
+		if (firstHand.at(ifh).value != -1)
+		{
+			totalScore += firstHand.at(ifh).value;
+		}
+		else
+		{
+			if (firstHand.at(ifh).special == "PLUSTWO")
+			{
+				totalScore += 30;
+			}
+			else if (firstHand.at(ifh).special == "REVERSE")
+			{
+				totalScore += 25;
+			}
+			else
+			{
+				totalScore += 20;
+			}
+		}
+		getScore(firstHand, secondHand, ifh + 1, ish);
+	}
+	// secondHand
+	if (ish < secondHand.size())
+	{
+		if (secondHand.at(ish).value != -1)
+		{
+			totalScore += secondHand.at(ish).value;
+		}
+		else
+		{
+			if (secondHand.at(ish).special == "PLUSTWO")
+			{
+				totalScore += 30;
+			}
+			else if (secondHand.at(ish).special == "REVERSE")
+			{
+				totalScore += 25;
+			}
+			else
+			{
+				totalScore += 20;
+			}
+		}
+		getScore(firstHand, secondHand, ifh, ish + 1);
+	}
+	return totalScore;
 }
 
-void drawCard(std::vector<Card> &player, std::vector<Card> &discardPile, std::vector<Card> &drawPile){
-    Card drawnCard = drawPile.at(0);
-    Card topCard = discardPile.at(0);
-    drawPile.erase(drawPile.begin());
+void drawCardAndPlay(std::vector<Card>& player, std::vector<Card>& discardPile, std::vector<Card>& drawPile) {
+	Card drawnCard = drawPile.at(0);
+	Card topCard = discardPile.at(0);
 
-    //Check if drawn card matches any values of the top card of discard pile
-    if(drawnCard.color == topCard.color || drawnCard.value == topCard.value || drawnCard.special == topCard.special)
-        discardPile.insert(discardPile.begin(),drawnCard);
-    else
-        player.insert(player.begin(),drawnCard);
-    //SORT CARDS AGAIN
+	drawPile.erase(drawPile.begin());
+
+	//Check if drawn card matches any values of the top card of discard pile
+	if (drawnCard.color == topCard.color || drawnCard.value == topCard.value || drawnCard.special == topCard.special)
+		discardPile.insert(discardPile.begin(), drawnCard);
+	else
+		player.insert(player.begin(), drawnCard);
+	//SORT CARDS AGAIN
+}
+
+void giveCardsAtStart(std::vector<Card>& player,  std::vector<Card>& drawPile) {
+	for (int i = 0; i < 7; i++)
+	{
+		Card drawnCard = drawPile.at(0);
+		player.insert(player.begin(), drawnCard);
+	}
 }
 
 
@@ -234,12 +247,14 @@ void printInstructivo() {
 void selectOptionOfMenu(int numberOfPlayer, std::vector<Card> actualPlayerDeck) {
 	int choice;
 	printMenu();
+
 	std::cin >> choice;
 
 	switch (choice) {
 	case 1:
 		std::cout << "You choose: DRAW\n";
 		// add function to draw card
+		drawCardAndPlay();
 		// draw card function() and in authomatic run function ifCardWorksPlayIt()
 		// finishTurn()
 		break;
@@ -255,12 +270,13 @@ void selectOptionOfMenu(int numberOfPlayer, std::vector<Card> actualPlayerDeck) 
 	case 3:
 		std::cout << "You choose: PLAYCARD\n";
 		// check if the the user had one card... if yes, punish him for not saying UNO
-        //no se que va adentro de playCard
+		//no se que va adentro de playCard
 		try {
-            playCard();
-        } catch(const char* msg) {
-            std::cout << msg << std::endl;
-        }
+			playCard();
+		}
+		catch (const char* msg) {
+			std::cout << msg << std::endl;
+		}
 
 		// check if win()
 		// finish Turn()
@@ -269,7 +285,7 @@ void selectOptionOfMenu(int numberOfPlayer, std::vector<Card> actualPlayerDeck) 
 	case 4:
 		std::cout << "You choose: HELP\n";
 		printInstructivo();
-		selectOptionOfMenu();
+		selectOptionOfMenu(numberOfPlayer, actualPlayerDeck);
 		break;
 
 	case 5:
@@ -279,7 +295,7 @@ void selectOptionOfMenu(int numberOfPlayer, std::vector<Card> actualPlayerDeck) 
 
 	default:
 		std::cout << "You choose: Error, option not recognized... try again.\n";
-		selectOptionOfMenu();
+		selectOptionOfMenu(numberOfPlayer, actualPlayerDeck);
 		break;
 	}
 
@@ -295,16 +311,16 @@ bool gameIsRunning(std::vector<Card> player, std::vector<Card> pc1, std::vector<
 }
 
 
-void playCard(std::string card, std::vector<Card> &player, std::vector<Card> &discardPile){
-    //For string Card ([RED2]) retrieve color type and number, and create new card.
-    std::string color = findColor(card);
-    std::string type = findType(card);
-    int value = findNumber(card);
-    Card mycard(color,value,type);
-    //Delete card from user's cards
-    player.erase(std::remove(player.begin(), player.end(), mycard), player.end());
-    //Add the new card(same card) to the discard pile
-    discardPile.insert(discardPile.begin(),mycard);
+void playCard(std::string card, std::vector<Card>& player, std::vector<Card>& discardPile) {
+	//For string Card ([RED2]) retrieve color type and number, and create new card.
+	std::string color = findColor(card);
+	std::string type = findType(card);
+	int value = findNumber(card);
+	Card mycard(color, value, type);
+	//Delete card from user's cards
+	player.erase(std::remove(player.begin(), player.end(), mycard), player.end());
+	//Add the new card(same card) to the discard pile
+	discardPile.insert(discardPile.begin(), mycard);
 }
 
 int main(int argc, char const* argv[])
@@ -314,11 +330,12 @@ int main(int argc, char const* argv[])
 	startGame(drawPile); //put 100 cards in the drawPile
 	shuffle(drawPile); // shuffle it
 
+	// discard pile
+	std::vector<Card> discardPile;
+
 	std::vector<Card> playerOne; //the human
 	std::vector<Card> cpuOne; //the pc1
 	std::vector<Card> cpuTwo; //the pc2
-
-
 
 	//give 7/7/7 cards to each player from the drawPile
 
@@ -326,11 +343,23 @@ int main(int argc, char const* argv[])
 	// start of game... //each player already has 7 cards...
 	int turn = 1; // 1 -> Player, 2 -> Pc1, 3 -> Pc2 
 	while (gameIsRunning(playerOne, cpuOne, cpuTwo)) {
+		// play
 
+		// see mi mano
+		std::cout << "Your hand:\n";
+		displayHand(playerOne);
+
+		// see cpu hands
+		std::cout << "Others hands:\n";
+		opponentsHand(cpuOne, 1);
+		opponentsHand(cpuTwo, 2);
+
+		// see the top card of the pile
+		std::cout << "Pile:\n";
+		displayTopCard(drawPile);
+
+		std::vector<Card> actualV = (turn == 1) ? playerOne : (turn == 2) ? cpuOne : cpuTwo;
+		selectOptionOfMenu(turn, actualV);
 	}
-
-	displayHand(drawPile);
-	opponentsHand(drawPile, 1);
-    displayTopCard(drawPile);
 	return 0;
 }
